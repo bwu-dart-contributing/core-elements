b9291111177ee5bf501daf527683c8ad95f33b8d
Günter Zöchbauer <guenter@gzoechbauer.com>
2014-08-05 13:10:06 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pub global deactivate bwu_testrunner
-pub global activate bwu_testrunner '>=0.0.2-3'
+pub global activate bwu_testrunner '>=0.0.2-4'
 pub global run bwu_testrunner:run -i
